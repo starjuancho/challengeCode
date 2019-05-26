b50376e179bc7f97f6c42045649e76c69399a4c2
Erick Garcia Ramirez <ericksupersonic1@gmail.com>
2019-05-26 13:55:12 -0500

    initial commit

---
+++ src/main/java/com/challenge/weatherSearch/WeatherSearchApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.weatherSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSearchApplication.class, args);
+	}
+
+}

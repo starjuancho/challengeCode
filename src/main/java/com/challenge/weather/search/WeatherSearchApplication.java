1d28139a42be446a37fac977f38d592e14ab8ed5
Erick Garcia Ramirez <ericksupersonic1@gmail.com>
2019-05-26 14:25:34 -0500

    reestructure of pacakges

--- src/main/java/com/challenge/weather/search/WeatherSearchApplication.java
+++ src/main/java/com/challenge/weather/search/WeatherSearchApplication.java
@@ -1,4 +1,4 @@
-package com.challenge.weatherSearch;
+package com.challenge.weather.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

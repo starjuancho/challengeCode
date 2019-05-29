de5c1dc9092555aed7c7017a001ef73a670c2679
Anzen Digital <ggerick88@gmail.com>
2019-05-29 16:21:44 -0500

    use of externalized variables

--- src/test/java/com/challenge/weather/search/client/impl/OpenWeatherClientImplTest.java
+++ src/test/java/com/challenge/weather/search/client/impl/OpenWeatherClientImplTest.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+
 import com.challenge.weather.search.client.model.CurrentWeather;
+import com.challenge.weather.search.constants.ConstantsConfig;
+import com.challenge.weather.search.constants.ConstantsTests;
 
 /**
  * Test class for testing the functionality of OpenWeatherClientImpl class.
@@ -22,6 +25,9 @@ public class OpenWeatherClientImplTest {
 
   @Mock
   RestTemplate restTemplate;
+  
+  @Mock
+  ConstantsConfig constantsConfig;
 
   @InjectMocks
   private OpenWeatherClientImpl empService = new OpenWeatherClientImpl();
@@ -38,6 +44,8 @@ public class OpenWeatherClientImplTest {
   @Test
   public void testRestCall() {
 
+	Mockito.when(constantsConfig.getUri()).thenReturn(ConstantsTests.URI);
+	Mockito.when(constantsConfig.getAppId()).thenReturn(ConstantsTests.APPID);
     Mockito.when(restTemplate.getForEntity(
         "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=6439103f2055efe2d2159bd06c670f5b",
         CurrentWeather.class)).thenReturn(new ResponseEntity<CurrentWeather>(cw, HttpStatus.OK));
